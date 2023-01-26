6f98eee9fce25b51d6c740583157e8deb51e0343
RiderMC126 <68037751+RiderMC126@users.noreply.github.com>
2023-01-26 20:15:03 +0300

    Create keyboard.py

---
+++ keyboard.py
@@ -0,0 +1,4 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+buy_lic = ReplyKeyboardMarkup(resize_keyboard=True)
+buy_lic.add(KeyboardButton(text='🔒 Приобрести доступ 🔒'))
